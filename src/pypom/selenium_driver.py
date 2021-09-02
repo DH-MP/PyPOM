94561ae9da2d0e9ed08b45fdd0f4ddc4b2f255d3
DH-MP <viet_mike.pham@hotmail.com>
2021-09-02 09:18:40 -0400

    Remove unsupported driver
    For selenium4

--- src/pypom/selenium_driver.py
+++ src/pypom/selenium_driver.py
@@ -5,14 +5,11 @@
 
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import (
-    Android,
-    BlackBerry,
     Chrome,
     Edge,
     Firefox,
     Ie,
     Opera,
-    PhantomJS,
     Remote,
     Safari,
 )
